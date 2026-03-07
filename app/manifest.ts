5cb3502bbd14efdbccb5be2fba35c697a9e96e7f
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2026-03-07 19:16:31 +0530

    added new icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/Icon-192.png",
+        src: "/Hab-Icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/Icon-512.png",
+        src: "/Hab-Icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
